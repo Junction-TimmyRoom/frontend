4b79cb6e9b1a1addc8449fdf37605629fec737db
westofsky <westofsky159@gmail.com>
2024-08-11 01:27:24 +0900

    chore: 주석 해제

--- src/pages/OnboardingPage.tsx
+++ src/pages/OnboardingPage.tsx
@@ -3,6 +3,7 @@ import { useEffect, useState } from 'react';
 import Phase1 from '@/components/onboarding/Phase1';
 import ProgressBar from '@/components/onboarding/ProgressBar';
 import Phase2 from '@/components/onboarding/Phase2';
+import { IconLogo } from '@/assets/icons';
 
 const OnboardingPage = () => {
   const [fadeOut, setFadeOut] = useState(false);
@@ -18,11 +19,11 @@ const OnboardingPage = () => {
 
   return (
     <>
-      {/* {!fadeOut && (
+      {!fadeOut && (
         <div className="w-full h-full left-0 absolute z-10 bg-navy flex justify-center items-end pb-12 transition-opacity duration-500">
           <IconLogo />
         </div>
-      )} */}
+      )}
       <div className="w-full h-screen pt-8">
         <ProgressBar phase={phase} />
         {phase === 1 ? <Phase1 setPhase={setPhase} /> : <Phase2 />}
