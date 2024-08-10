063f3383a470ed5642121241f72c5499d48dde7e
westofsky <westofsky159@gmail.com>
2024-08-11 01:18:36 +0900

    feat: 디테일 수정

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,3 +1,6 @@
 export { ReactComponent as IconBack } from './Back.svg';
 export { ReactComponent as IconSearch } from './icon_search.svg';
 export { ReactComponent as IconCamera } from './icon_camera.svg';
+export { ReactComponent as IconWarn } from './icon_warn.svg';
+export { ReactComponent as IconArrowBottom } from './icon_arrow_bottom.svg';
+export { ReactComponent as IconArrowTop } from './icon_arrow_top.svg';
--- src/pages/CommentPage.tsx
+++ src/pages/CommentPage.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 const CommentPage = () => {
   return <>메뉴 페이지입니다.</>;
 };
